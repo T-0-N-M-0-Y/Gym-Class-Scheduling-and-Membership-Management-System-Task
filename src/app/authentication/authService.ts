19ed5483510756cfaad1c434e376f4f0cd280e05
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2025-06-19 18:50:54 +0600

    Added error handler

--- src/app/authentication/authService.ts
+++ src/app/authentication/authService.ts
@@ -3,15 +3,16 @@ import { TLoginResponse, TLoginUser } from './authInterface';
 import { UserModel } from '../user/userModel';
 import { Utils } from '../Utils';
 import config from '../config';
+import { ApiError } from '../apiError';
 
 const loginUser = async (payload: TLoginUser): Promise<TLoginResponse> => {
     const { email, password } = payload;
 
     const user = await UserModel.findOne({ email });
-    if (!user) throw new Error('User not found');
+    if (!user) throw new ApiError(401, 'User not found');
 
     const isPasswordValid = await Utils.comparePassword(password, user.password);
-    if (!isPasswordValid) throw new Error('Invalid credentials');
+    if (!isPasswordValid) throw new ApiError(500, 'Invalid credentials');
 
     const accessToken = jwt.sign(
         { _id: user._id, role: user.role, email: user.email },
